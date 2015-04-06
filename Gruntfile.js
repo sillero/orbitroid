e7c6c623e4b8a2337ec36a35b55891e3b54df037
Gustavo Sillero <gustavo@sille.ro>
2015-04-05 23:08:23 -0300

    better bower task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,24 @@
 var bower = require('bower');
+var inquirer = require('inquirer');
 
 module.exports = function(grunt) {
   grunt.initConfig({
-    connect: {
-      server: {
-        options: {
-          livereload: true
-        }
-      }
-    },
-    watch: {
-      livereload: {
-        files: '**/*',
-        tasks: [],
-        options: {
-          livereload: true
-        }
-      }
+  connect: {
+    server: {
+    options: {
+      livereload: true
     }
+    }
+  },
+  watch: {
+    livereload: {
+    files: '**/*',
+    tasks: [],
+    options: {
+      livereload: true
+    }
+    }
+  }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
@@ -26,12 +27,38 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['bower', 'connect', 'watch']);
 
   grunt.registerTask('bower', function(){
-    var done = this.async();
+  var done = this.async();
+  var bowerOptions = {
+    directory: 'js/vendor',
+    interactive: true
+  };
 
-    bower.commands
-      .install(['fetch'], {}, { directory: 'js/vendor' })
-      .on('end', function(installed){
-          done();
-      });
+  bower.commands
+    .install(['fetch', 'three.js'], {}, bowerOptions)
+    .on('log', function(log){
+      grunt.log.ok('bower:', log.message);
+    })
+    .on('end', function(installed){
+      installed = Object.keys(installed);
+
+      if (installed.length) {
+        installed = 'installed:' + installed.join(', ');
+      }
+      else {
+        installed = 'no package installed';
+      }
+
+      grunt.log.ok(installed);
+      done();
+    })
+    .on('error', function(error){
+      grunt.log.error(error);
+      done(error);
+    })
+    .on('prompt', function(prompt, callback){
+      grunt.log.ok(prompt);
+      inquirer.prompt(prompts, callback);
+    });
+
   });
 };
