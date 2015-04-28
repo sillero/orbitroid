fc3be3e3637cad139c1416eaef2b1d0e479df6e3
Gustavo Sillero <sillero@gmail.com>
2015-04-28 18:12:47 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
   ///--------------------------
   ///--------------------------
 
-  var orbitroide = new Game(document.getElementById('WebGLCanvas'));
+  var orbitroid = new Game(document.getElementById('WebGLCanvas'));
   // var gui = new dat.GUI();
 
   orbitroid
